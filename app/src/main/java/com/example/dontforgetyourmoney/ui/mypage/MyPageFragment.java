c92f8683a8f6792e6fc397be69117e2b353d0d67
AppleJuice87 <mujin0305@naver.com>
2024-11-17 19:34:02 +0900

    first commit

---
+++ app/src/main/java/com/example/dontforgetyourmoney/ui/mypage/MyPageFragment.java
@@ -0,0 +1,24 @@
+package com.example.dontforgetyourmoney.ui.mypage;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.dontforgetyourmoney.R;
+
+public class MyPageFragment extends Fragment {
+
+    public MyPageFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mypage, container, false);
+    }
+}
